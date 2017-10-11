4643fbaf90ca906d2dab43a6e3370b263cc0beb5
W. Ryan Parker <rparker@indeed.com>
2017-10-11 16:37:16 -0500

    Generated random db seed with Faker and finished index listing for blogging system.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'welcome/index'
+  
+  resources :articles  #resources is plural... omg
+  
   root 'welcome#index'
-
-  resource :articles
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+include Faker
+
+50.times do
+  Article.create(
+    title: 'REPORT: ' + Faker::Space.nebula + ' in ' + Faker::Space.galaxy,
+    body: Faker::Hipster.paragraphs(30, true),
+    author: Faker::LordOfTheRings.character,
+    location: Faker::LordOfTheRings.location
+  )
+end
