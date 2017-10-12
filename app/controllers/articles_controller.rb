83b5d5a95d12ade817e201dcfb68512c1c4af087
W. Ryan Parker <rparker@indeed.com>
2017-10-12 11:30:29 -0500

    basic functionality added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,5 +6,39 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+  
+  def edit
+    @article = Article.find(params[:id])
+  end
+  
+  def update
+      @article = Article.find(params[:id])
+      
+      if @article.update(article_params)
+        redirect_to @article
+      else
+        render 'edit'
+      end
+  end
+  
+  def new
+    @article = Article.new
+  end
+  
+  def create
+    #render plain:params[:article].inspect
+    #Above is for checking it out.
+    @article =Article.new(article_params)
+    if @article.save
+      redirect_to @article
+    else
+        render 'new'
+    end
+    
+  end
 
+  private
+    def article_params
+      params.require(:article).permit(:title, :body)
+    end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,6 @@
 class Article < ApplicationRecord
+  #validation goes in the model.
+  validates :title, presence: true,
+                    length: { minimum:5 }
+  validates :body, presence: true
 end
